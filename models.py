d3b83288cca4a58e6697282cb6ab8d8d73ca14fa
frunzadenis.93@gmail.com <frunzadenis.93@gmail.com>
2019-07-02 08:49:44 +0300

    initial commits

---
+++ models.py
@@ -0,0 +1,57 @@
+import datetime
+
+#from flask.ext.bcrypt import generate_password_hash, check_password_hash
+from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
+#from flask.ext.login import UserMixin
+from flask_login import UserMixin
+from peewee import *
+
+
+DATABASE = SqliteDatabase('social.db')
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+    bio = TextField(default='')
+
+    class Meta:
+        database = DATABASE
+
+    def get_posts(self):
+        return Post.select().where(Post.user == self)
+
+    def get_stream(self):
+        return Post.select().where(
+            (Post.user == self)
+            )
+    
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
+        except IntegrityError:
+            raise ValueError("User already exists")
+
+
+class Post(Model):
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    user = ForeignKeyField(
+        model = User,
+        backref = 'posts'
+        )
+    content = TextField()
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-timestamp',)
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    DATABASE.close()
